48ee9197e37e703970c4a0cfd146846fed01df43
Potter <aa4790139@gmail.com>
2024-09-20 09:58:07 +0800

    chore: update playground

--- playground/node.js
+++ playground/node.js
@@ -1,4 +1,4 @@
-import { translator, engines } from "../dist/index.js"
+import { translator, engines } from "../dist/node/index.js"
 
 translator.use(engines.google());
 const res = await translator.translate("hello", { from: "en", to: "zh" });

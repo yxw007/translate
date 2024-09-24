51812376b7327be64c45415bc04c08ec43458bd7
Potter <aa4790139@gmail.com>
2024-09-24 12:13:14 +0800

    fix: update translation test timeout

--- test/translate.test.ts
+++ test/translate.test.ts
@@ -15,7 +15,7 @@ describe("translator", () => {
     //use cache
     const start = Date.now();
     const res3 = await translator.translate("hello", { from: "en", to: "Chinese", engine: "google" });
-    expect(Date.now() - start).toBeLessThan(1);
+    expect(Date.now() - start).toBeLessThan(2);
     expect(res3).toEqual(["你好"]);
   });
 

675cf2b392607f1dfc86f402cc33212d0a44aa30
Potter <75838259@qq.com>
2024-12-09 11:42:17 +0800

    fix(deepl): bad Request Parameter 'text' not specified

--- src/engines/deepl.ts
+++ src/engines/deepl.ts
@@ -30,25 +30,29 @@ export function deepl(options: DeeplEngineOption) {
       if (!Array.isArray(text)) {
         text = [text];
       }
-      const requestBody = JSON.stringify({
-        text,
-        source_lang: from === "auto" ? undefined : from,
-        target_lang: to,
-      });
       const res: any = await fetch(url, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json; charset=UTF-8;",
+          "Content-Type": "application/json; charset=UTF-8",
           Authorization: `DeepL-Auth-Key ${key}`,
+          Accept: "*/*",
+          Host: "api-free.deepl.com",
           Connection: "keep-alive",
         },
-        body: requestBody,
+        body: JSON.stringify({
+          text: text,
+          source_lang: from === "auto" ? undefined : from,
+          target_lang: to,
+        }),
       });
       if (!res.ok) {
         throw await throwResponseError(this.name, res);
       }
-      const bodyRes = await res.json();
-      const body: Translation[] = bodyRes?.translations;
+      const bodyRes = await (res as any).json();
+      if (bodyRes.error) {
+        throw new TranslationError(this.name, `Translate fail ! code: ${bodyRes.error.code}, message: ${bodyRes.error.message}`);
+      }
+      const body: Translation[] = bodyRes.translations;
       if (!body || body.length === 0) {
         throw new TranslationError(this.name, "Translate fail ! translate's result is null or empty");
       }
